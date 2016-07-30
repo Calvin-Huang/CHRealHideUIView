dd507fe9101af1759a222b241c99ce7611983e8c
Calvin <calvin.peak@capslock.tw>
2016-07-30 20:17:31 +0900

    Add storyboard layout and connect to code.

--- CHRealHideUIView/ViewController.swift
+++ CHRealHideUIView/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func toggleHideAndShowButtonClicked(_: AnyObject) {
+        
+    }
 }
 
