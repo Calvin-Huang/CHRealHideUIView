32ecba5d81dfa95c2bfdab21906639bb44f5d257
Calvin <calvin.peak@capslock.tw>
2016-07-30 19:34:42 +0900

    Add requires files

---
+++ CHRealHideUIView/UIViewController+RealHideViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+RealHideViewExtension.swift
+//  CHRealHideUIView
+//
+//  Created by Calvin on 7/30/16.
+//  Copyright © 2016 CapsLock. All rights reserved.
+//
+
+import Foundation
